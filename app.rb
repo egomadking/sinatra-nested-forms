644b909e12fe800ee8b2e87e40d45de97cebb6d1
Roy Mosby <ego.madking@gmail.com>
2021-01-26 17:43:29 -0500

    commit tree 0bf01b0670399ec3d0fb21615f99cc8cbc992e07

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+      erb :root
+    end
+
+    get "/new" do
+      erb :"pirates/new"
+    end
+
+    post "/pirates" do
+      pirate = params[:pirate]
+      ships = params[:pirate][:ships]
+      @new_pirate = Pirate.new(name: pirate[:name], weight: pirate[:weight], height: pirate[:height])
+      @new_ships = []
+      ships.each do |ship|
+        @new_ships << Ship.new(name: ship[:name], type: ship[:type], booty: ship[:booty])
+      end
+      erb :"pirates/show"
+    end
 
   end
 end
